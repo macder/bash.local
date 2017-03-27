fce7a0cd44702a0e2381f443892d09f7cbdb0bfd
Maciej Derulski <macder7@gmail.com>
2017-03-26 21:44:39 -0400

    removing sudo from commands - should be executed as sudo

--- install/install.post_os..sh
+++ install/install.post_os..sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Install system packages after installing ubuntu base (mini.iso)
+# Run this after installing a minimalist Ubuntu (mini.iso w/ manual package selection)
+# sudo ./install.post_os.sh
 
 # this is a WIP
 
 declare APT_DESKTOP=( 
-  # "--no-install-recommends ubuntu-gnome-desktop"
+  # "--no-install-recommends ubuntu-gnome-desktop" # Needs to be seperated, dont want others to not install recommends
   "xserver-xorg-legacy"
   "gksu"
   "gnome-software"
@@ -41,28 +42,31 @@ declare BLACKLIST=(
 function grub_text {
   file="/etc/default/grub"
 
-  sudo sed -i 's/quite splash//g' $file
-  sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' $file
-  sudo echo "GRUB_GFXPAYLOAD=text" >> $file
+  sed -i 's/quite splash//g' $file
+  sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' $file
+  echo "GRUB_GFXPAYLOAD=text" >> $file
 
-  sudo nano $file
-  sudo update-grub
+  nano $file
+  update-grub
 }
 
 function network {
   file="/etc/network/interfaces"
 
-  sudo sed -i 's/auto eno1/allow-hotplug eno1/g' $file
-  sudo nano $file
+  sed -i 's/auto eno1/allow-hotplug eno1/g' $file
+  nano $file
 
-  sudo systemctl disable network-manger
-  sudo systemctl disable networking.service
+  systemctl disable network-manger
+  systemctl disable networking.service
 }
 
 function modprobe_blacklist {
-  sudo echo "/etc/modprobe.d/intel.conf" > /etc/modprobe.d/intel.conf
+  echo "options snd_hda_intel enable=1,0" > /etc/modprobe.d/intel.conf
 
   # TODO - bring in the blacklist array
-  # sudo echo -e "$blacklist" >> /etc/modprobe.d/blacklist.conf
-  sudo update-initramfs -u
+  # echo -e "$blacklist" >> /etc/modprobe.d/blacklist.conf
+  update-initramfs -u
 }
+
+#echo fs.inotify.max_user_watches=16384 | tee -a /etc/sysctl.conf
+#sysctl -p
